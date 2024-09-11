134d594e92bff85cfd2def27eaa025caefb592e5
tian <1013515028@qq.com>
2024-09-11 15:33:25 +0800

    add chat template

--- transformers/vlm_chat.py
+++ transformers/vlm_chat.py
@@ -76,7 +76,10 @@ if __name__ == '__main__':
             if is_img:
                 response = '请根据图片提问'
             else:
-                input_prompt = f'USER: <image>\n{prompt}\nASSISTANT:'
+                content_arr = [{'type': 'text', 'text': prompt}]
+                if st.session_state.img is not None: content_arr.append({'type': 'image'})
+                messages = [{'role': 'user', 'content': content_arr}]
+                input_prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
                 inputs = processor(input_prompt, st.session_state.img, return_tensors='pt').to(model.device, torch.float16)
                 outputs = model.generate(**inputs, max_new_tokens=512, do_sample=False)
                 response = processor.decode(outputs[0][inputs['input_ids'].shape[-1]:], skip_special_tokens=True)
